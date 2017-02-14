57e62873ae6b0a2b4454e37c2e2549ffbbf90525
girishbathala <girish.bathala@gmail.com>
2017-02-13 19:44:25 -0800

    Python 3 Compatible

--- utils/BatchFilleNames_Output.py
+++ utils/BatchFilleNames_Output.py
@@ -23,10 +23,10 @@ numSegments = 1000
 list_files = os.listdir(rgb_dir)
 list_files.sort()
 
-for block in xrange(0,14):
+for block in range(0,14):
     list_start = block * 1000
     list_end = (block+1) * 1000 -1
-    print '{} : {}'.format(list_start, list_files[list_start])
+    print ('{} : {}'.format(list_start, list_files[list_start]))
     if list_end > len(list_files):
         list_end = len(list_files)-1
-    print '{} : {}'.format(list_end, list_files[list_end])
+    print ('{} : {}'.format(list_end, list_files[list_end]))
